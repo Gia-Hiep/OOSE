cbe6d544a89ab1bc9d91b0cc99ac93c1219743d4
Gia-Hiep <hiepprook9@gmail.com>
2025-10-19 09:23:21 +0700

    Add demo_MVC

--- demo_MVC/src/main/java/com/demo/mvc/entity/Product.java
+++ demo_MVC/src/main/java/com/demo/mvc/entity/Product.java
@@ -8,9 +8,9 @@ import java.math.BigDecimal;
 public class Product {
   @Id @GeneratedValue(strategy=GenerationType.IDENTITY)
   private Integer product_id;
-  @Column(nullable=false, precision=19, scale=4)
+  @Column(nullable=false)
   private BigDecimal price;
-  @Column(nullable=false, precision=4, scale=2)
+  @Column(nullable=false)
   private BigDecimal weight;
   @ManyToOne @JoinColumn(name="product_category_id")
   private ProductCategory category;
--- demo_MVC/src/main/java/com/demo/mvc/repository/ProductRepository.java
+++ demo_MVC/src/main/java/com/demo/mvc/repository/ProductRepository.java
@@ -1,3 +1,29 @@
 package com.demo.mvc.repository;
-import com.demo.mvc.entity.Product; import org.springframework.data.jpa.repository.JpaRepository;
-public interface ProductRepository extends JpaRepository<Product, Integer> { }
+
+import com.demo.mvc.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface ProductRepository extends JpaRepository<Product, Integer> {
+
+    @Query(value = """
+        SELECT 
+            p.product_id,
+            pt.product_name,
+            pct.category_name,
+            p.price,
+            p.weight
+        FROM products p
+        LEFT JOIN product_translation pt
+            ON p.product_id = pt.product_id AND pt.language_id = :lang
+        LEFT JOIN product_category pc
+            ON p.product_category_id = pc.product_category_id
+        LEFT JOIN product_category_translation pct
+            ON pc.product_category_id = pct.product_category_id AND pct.language_id = :lang
+        """, nativeQuery = true)
+
+    List<Object[]> findProductsByLanguage(@Param("lang") String lang);
+}
